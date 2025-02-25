f84caf7ee2503b7afb57dfe0b2ecead18d9f6efd
SachithKumar0628 <sachith.k@s.amity.edu>
2025-02-25 10:34:13 +0530

    Third Commit

--- swap.c
+++ swap.c
@@ -3,16 +3,16 @@
 int main() {
     int a, b; 
 
-    // Input from user
+    
     printf("Enter two numbers: ");
     scanf("%d %d", &a, &b);
 
-    // Swapping using a temporary variable
+    
     a = a + b;
     b = a - b;
     a = a - b;
 
-    // Display result
+    
     printf("After swapping: a = %d, b = %d\n", a, b);
 
     return 0;
