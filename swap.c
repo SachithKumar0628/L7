37ccd6d68410e1da9c45f203c8f9d917590e8dc4
SachithKumar0628 <sachith.k@s.amity.edu>
2025-02-25 10:25:04 +0530

    Without temp variable

--- swap.c
+++ swap.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    int a, b, temp;
+    int a, b; 
 
     // Input from user
     printf("Enter two numbers: ");
     scanf("%d %d", &a, &b);
 
     // Swapping using a temporary variable
-    temp = a;
-    a = b;
-    b = temp;
+    a = a + b;
+    b = a - b;
+    a = a - b;
 
     // Display result
     printf("After swapping: a = %d, b = %d\n", a, b);
