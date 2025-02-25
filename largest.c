1e2f2d6483a491fd75e029cc78058e3b3eaedb41
SachithKumar0628 <sachith.k@s.amity.edu>
2025-02-25 11:34:00 +0530

    Fifth Commit TV removed comments

--- largest.c
+++ largest.c
@@ -3,14 +3,14 @@
 int main() {
     int num1, num2, largest;
 
-    // Input two numbers
+    
     printf("Enter two numbers: ");
     scanf("%d %d", &num1, &num2);
 
-    // Using ternary operator to find the largest number
+    
     largest = (num1 > num2) ? num1 : num2;
 
-    // Display result
+    
     printf("The largest number is: %d\n", largest);
 
     return 0;
